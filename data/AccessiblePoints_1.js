43c25f43106c577a4481b25547b34bda09c7b199
AccessLab <54842392+AccessLabTeam@users.noreply.github.com>
2025-02-01 00:14:14 +0200

    Add files via upload

---
+++ data/AccessiblePoints_1.js
@@ -0,0 +1,14 @@
+var json_AccessiblePoints_1 = {
+"type": "FeatureCollection",
+"name": "AccessiblePoints_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Name": "Town Hall" }, "geometry": { "type": "Point", "coordinates": [ 24.942847387016311, 37.444707737750157 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Name": "Port of Syros" }, "geometry": { "type": "Point", "coordinates": [ 24.941277576393411, 37.440998017274659 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Name": "Accessible Parking" }, "geometry": { "type": "Point", "coordinates": [ 24.945218008347119, 37.441826963548493 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Name": "Shopping Center" }, "geometry": { "type": "Point", "coordinates": [ 24.942373699623264, 37.443692637946008 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Name": "Restaurants - Cafes" }, "geometry": { "type": "Point", "coordinates": [ 24.944138203352878, 37.443419796459651 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Name": "Accessible Hotel" }, "geometry": { "type": "Point", "coordinates": [ 24.945501990624258, 37.442748995242944 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Name": "Apollon Theatre" }, "geometry": { "type": "Point", "coordinates": [ 24.943695180383621, 37.445560637959026 ] } }
+]
+}
